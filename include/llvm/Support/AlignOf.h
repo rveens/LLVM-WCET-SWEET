0f8722c1f2d30f73e4ee82655558ff7b2f88e3aa
hans <hans@91177308-0d34-0410-b5e6-96231b3b80d8>
2017-01-12 21:25:21 +0000

    Creating release_40 branch off revision 291814
    git-svn-id: http://llvm.org/svn/llvm-project/llvm/branches/release_40@291816 91177308-0d34-0410-b5e6-96231b3b80d8

---
+++ include/llvm/Support/AlignOf.h
@@ -0,0 +1,138 @@
+//===--- AlignOf.h - Portable calculation of type alignment -----*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines the AlignedCharArray and AlignedCharArrayUnion classes.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_SUPPORT_ALIGNOF_H
+#define LLVM_SUPPORT_ALIGNOF_H
+
+#include "llvm/Support/Compiler.h"
+#include <cstddef>
+
+namespace llvm {
+
+/// \struct AlignedCharArray
+/// \brief Helper for building an aligned character array type.
+///
+/// This template is used to explicitly build up a collection of aligned
+/// character array types. We have to build these up using a macro and explicit
+/// specialization to cope with MSVC (at least till 2015) where only an
+/// integer literal can be used to specify an alignment constraint. Once built
+/// up here, we can then begin to indirect between these using normal C++
+/// template parameters.
+
+// MSVC requires special handling here.
+#ifndef _MSC_VER
+
+template<std::size_t Alignment, std::size_t Size>
+struct AlignedCharArray {
+  LLVM_ALIGNAS(Alignment) char buffer[Size];
+};
+
+#else // _MSC_VER
+
+/// \brief Create a type with an aligned char buffer.
+template<std::size_t Alignment, std::size_t Size>
+struct AlignedCharArray;
+
+// We provide special variations of this template for the most common
+// alignments because __declspec(align(...)) doesn't actually work when it is
+// a member of a by-value function argument in MSVC, even if the alignment
+// request is something reasonably like 8-byte or 16-byte. Note that we can't
+// even include the declspec with the union that forces the alignment because
+// MSVC warns on the existence of the declspec despite the union member forcing
+// proper alignment.
+
+template<std::size_t Size>
+struct AlignedCharArray<1, Size> {
+  union {
+    char aligned;
+    char buffer[Size];
+  };
+};
+
+template<std::size_t Size>
+struct AlignedCharArray<2, Size> {
+  union {
+    short aligned;
+    char buffer[Size];
+  };
+};
+
+template<std::size_t Size>
+struct AlignedCharArray<4, Size> {
+  union {
+    int aligned;
+    char buffer[Size];
+  };
+};
+
+template<std::size_t Size>
+struct AlignedCharArray<8, Size> {
+  union {
+    double aligned;
+    char buffer[Size];
+  };
+};
+
+
+// The rest of these are provided with a __declspec(align(...)) and we simply
+// can't pass them by-value as function arguments on MSVC.
+
+#define LLVM_ALIGNEDCHARARRAY_TEMPLATE_ALIGNMENT(x) \
+  template<std::size_t Size> \
+  struct AlignedCharArray<x, Size> { \
+    __declspec(align(x)) char buffer[Size]; \
+  };
+
+LLVM_ALIGNEDCHARARRAY_TEMPLATE_ALIGNMENT(16)
+LLVM_ALIGNEDCHARARRAY_TEMPLATE_ALIGNMENT(32)
+LLVM_ALIGNEDCHARARRAY_TEMPLATE_ALIGNMENT(64)
+LLVM_ALIGNEDCHARARRAY_TEMPLATE_ALIGNMENT(128)
+
+#undef LLVM_ALIGNEDCHARARRAY_TEMPLATE_ALIGNMENT
+
+#endif // _MSC_VER
+
+// This code implements the equivalent of std::aligned_union from C++11.
+// That is supported by Visual Studio 2015 and GCC 5.1.
+// Once these are the baselines for LLVM, we can use std::aligned_union instead.
+
+namespace support {
+namespace detail {
+template <typename T1> constexpr size_t aligner() { return alignof(T1); }
+
+template <typename T1, typename T2, typename... Ts> constexpr size_t aligner() {
+  return (alignof(T1) > aligner<T2, Ts...>()) ? alignof(T1)
+                                              : aligner<T2, Ts...>();
+}
+
+template <typename T1> constexpr size_t sizer() { return sizeof(T1); }
+
+template <typename T1, typename T2, typename... Ts> constexpr size_t sizer() {
+  return (sizeof(T1) > sizer<T2, Ts...>()) ? sizeof(T1) : sizer<T2, Ts...>();
+}
+} // end namespace detail
+} // end namespace support
+
+/// \brief This union template exposes a suitably aligned and sized character
+/// array member which can hold elements of any of a number of types.
+///
+/// These types may be arrays, structs, or any other types. The goal is to
+/// expose a char array buffer member which can be used as suitable storage for
+/// a placement new of any of these types.
+template <typename... Ts>
+struct AlignedCharArrayUnion
+    : llvm::AlignedCharArray<support::detail::aligner<Ts...>(),
+                             support::detail::sizer<Ts...>()> {};
+} // end namespace llvm
+
+#endif // LLVM_SUPPORT_ALIGNOF_H
